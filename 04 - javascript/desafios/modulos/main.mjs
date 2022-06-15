095dcf273309683666d87cc1bb768bc9e43975f8
andrenuncio <andrenuncio@gmail.com>
2022-06-15 11:32:04 -0300

    Adiciona arquivos utilizados no estudo de modulos javascript

---
+++ 04 - javascript/desafios/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraHobby , mostraIdade } from "./funcoes.mjs";
+
+console.log(mostraIdade("Badabimga", 99));
+console.log(mostraCidade("Badabimga","Cyudade"));
+console.log(mostraHobby("124","javascript"));
