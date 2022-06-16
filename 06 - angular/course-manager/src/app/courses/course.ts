58605273c5867c1692610ae4ef4f2965babf7c54
andrenuncio <andrenuncio@gmail.com>
2022-06-16 18:46:06 -0300

    Primeiro commit do curse de Angular, com o primeior modulo cirado

---
+++ 06 - angular/course-manager/src/app/courses/course.ts
@@ -0,0 +1,21 @@
+export class Course {
+    id: number = 0;
+    name: string = 'Nome do Curso';
+    imageUrl: string = '';
+    price: number = 999999;
+    code: string = 'CODIGO-CURSO';
+    duration: number = 0;
+    rating: number = 5;
+    releaseDate: string = 'Mes, Dia, Ano';
+}
+
+// export class Course {
+//     id: number;
+//     name: string;
+//     imageUrl: string;
+//     price: number;
+//     code: string;
+//     duration: number;
+//     rating: number;
+//     releaseDate: string;
+// }
