2b4f1e343825ad1beef932291711a4c61004cdf7
andrenuncio <andrenuncio@gmail.com>
2022-06-28 20:48:30 -0300

    Projeto de cadastro de series via console

---
+++ 07 - C#/FILE_AND_FILEINFO/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;  // Nao precisa pq ja tem no implicit usings
+using static System.Console;
+
+WriteLine("Digite o nome do arquivo: ");
+string name = ReadLine();
+
+while(name == null || name == "")
+{
+    WriteLine("Digite um nome valido: ");
+    name = ReadLine();
+}
+
+string path = Path.Combine(Environment.CurrentDirectory , $"{name}.txt");
+
+CreateFile(path);
+
+WriteLine("Aperte Enter para finalizar...");
+ReadLine();
+
+static void CreateFile(string path)
+{
+
+    try
+    {
+        using (StreamWriter sw = File.CreateText(path))
+    {
+        sw.WriteLine("Linha 1");
+        sw.WriteLine("Linha 2");
+        sw.WriteLine("Linha 3");
+    }
+    }
+    catch 
+    {
+        
+        WriteLine("O Nome do arquivo esta invalido!!!");
+    }
+    
+}
+
+
+
+
+
+
+
